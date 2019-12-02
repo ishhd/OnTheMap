f9965d2140d7635ffaed465ceb9facfebada278b
Shahad <Shahadabdualah@gmail.com>
2019-12-02 09:23:54 +0300

    first commit

---
+++ OnTheMap/Parse.swift
@@ -0,0 +1,62 @@
+//
+//  Parse.swift
+//  OnTheMap
+//
+//  Created by Shahad on 19/03/1441 AH.
+//  Copyright © 1441 Udacity. All rights reserved.
+//
+
+import Foundation
+
+class Parse {
+    
+    
+    static func postStudentLocation(_ location : StudentLocation, completion: @escaping (Error?)-> Void) {
+        
+        var request = URLRequest(url: URL(string: "https://onthemap-api.udacity.com/v1/StudentLocation")!)
+        request.httpMethod = "POST"
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpBody = "{\"uniqueKey\": \"1234\", \"firstName\": \"John\", \"lastName\": \"Doe\",\"mapString\": \"Mountain View, CA\", \"mediaURL\": \"https://udacity.com\",\"latitude\": 37.386052, \"longitude\": -122.083851}".data(using: .utf8)
+        let session = URLSession.shared
+        let task = session.dataTask(with: request) { data, response, error in
+            if error != nil { // Handle error…
+                return
+            }
+            print(String(data: data!, encoding: .utf8)!)
+        }
+        task.resume()
+    }
+    
+    static func getStudentLocation(limit : Int = 100, skip:Int = 0, orderBy: SLParam = .updatedAt ,completion: @escaping (Error?)->()){
+        
+        var request = URLRequest(url: URL(string: "https://onthemap-api.udacity.com/v1/StudentLocation?order=-updatedAt")!)
+        let session = URLSession.shared
+        let task = session.dataTask(with: request) { data, response, error in
+            if error != nil { // Handle error...
+                return
+            }
+            print(String(data: data!, encoding: .utf8)!)
+        }
+        task.resume()
+        
+    }
+    static func putStudentLocation(objectId: String , completion: @escaping (Error?)->()){
+        let urlString = "https://onthemap-api.udacity.com/v1/StudentLocation/8ZExGR5uX8"
+        let url = URL(string: urlString)
+        var request = URLRequest(url: url!)
+        request.httpMethod = "PUT"
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpBody = "{\"uniqueKey\": \"1234\", \"firstName\": \"John\", \"lastName\": \"Doe\",\"mapString\": \"Cupertino, CA\", \"mediaURL\": \"https://udacity.com\",\"latitude\": 37.322998, \"longitude\": -122.032182}".data(using: .utf8)
+        let session = URLSession.shared
+        let task = session.dataTask(with: request) { data, response, error in
+            if error != nil { // Handle error…
+                return
+            }
+            print(String(data: data!, encoding: .utf8)!)
+        }
+        task.resume()
+        
+    }
+    
+}
+
